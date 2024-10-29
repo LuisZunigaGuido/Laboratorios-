b3aa2ade6a5659219fc66d4b8c8ec5ab61d5840b
LuisZunigaGuido <luis.guido1507@gmail.com>
2024-10-29 17:34:18 -0600

    fin del lab del formulario

--- Laboratorio Formulario de Java Swing/VentanaPrincipal.java
+++ Laboratorio Formulario de Java Swing/VentanaPrincipal.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.awt.event.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class VentanaPrincipal extends JFrame {
     private JTextField campoNombre;
     private JTextField campoEmail;
     private JTextField campoTelefono;
+    private JButton botonGuardar;
 
     public VentanaPrincipal() {
         setTitle("Formulario del usuario");
@@ -51,19 +54,13 @@ public class VentanaPrincipal extends JFrame {
         panelFormulario.add(campoTelefono);
         add(panelFormulario);
 
-        // JComboBox que tenga opciones de usuario
-        String[] items = {"Cliente", "Empleado", "Administrador"};
-        JComboBox<String> comboBox = new JComboBox<>(items);
-        comboBox.setName("Opciones de usuario");
-        panelFormulario.add(comboBox);
-        comboBox.setVisible(true);
-        
         //boton de Guardar
         JButton botonGuardar = new JButton("Guardar");
-        botonGuardar.addActionListener(e -> {
         String Nombre = campoNombre.getText();
         String Email = campoEmail.getText();
         String Telefono = campoTelefono.getText();
+        botonGuardar.setVisible(false);
+        botonGuardar.addActionListener(e -> {
         try {
             FileWriter writer = new FileWriter("datos_usuario.txt", true);
             writer.write("nombre: " + Nombre + ", email: " + Email + ", Telefono: " + Telefono + "\n");
@@ -72,30 +69,41 @@ public class VentanaPrincipal extends JFrame {
             JOptionPane.showMessageDialog(null, "Datos guardados exitosamente.");
         } catch (IOException ex) {
             JOptionPane.showMessageDialog(null, "Error al guardar los archivos.");
-        }
-        });
-
+        }});
+        
         // JCheckBox para aceptar términos y condiciones
         JCheckBox checkBox = new JCheckBox("He leído y acepto los términos y condiciones previamente señalados");
         panelFormulario.add(checkBox);
         checkBox.setVisible(true);
-
+        
+        // JComboBox que tenga opciones de usuario
+        String[] items = {"Cliente", "Empleado", "Administrador"};
+        JComboBox<String> comboBox = new JComboBox<>(items);
+        comboBox.addActionListener(e-> {
+           botonGuardar.setVisible(checkBox.isSelected()); 
+        }
+        );
+        comboBox.setName("Opciones de usuario");
+        panelFormulario.add(comboBox);
+        comboBox.setVisible(true);
+        
         // Botón de limpiar
         JButton botonLimpiar = new JButton("Limpiar");
         botonLimpiar.addActionListener(e -> {
-        campoNombre.setText("");
-        campoEmail.setText("");
-        campoTelefono.setText("");
-        checkBox.setSelected(false);
-        JOptionPane.showMessageDialog(null, "los datos fueron limpiados, inserte nuevos datos.");
+            campoNombre.setText("");
+            campoEmail.setText("");
+            campoTelefono.setText("");
+            checkBox.setSelected(true);
+            JOptionPane.showMessageDialog(null, "los datos fueron limpiados, inserte nuevos datos.");
         });
+        
 
         panelFormulario.setLayout(new GridLayout(0, 1)); //deseo poner los botones a la par y moderados pero me conformo por el momento.
         panelFormulario.add(botonGuardar);
         panelFormulario.add(botonLimpiar);
     }
-
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new VentanaPrincipal().setVisible(true));
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
     }
 }
